4e955f463e5dfcf02095f4c9e0216229055ff2a1
eladmallel <eladmallel@gmail.com>
2026-01-28 12:06:10 -0500

    fix: e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: `npm run dev -- --port ${TEST_PORT}`,
+    command: `npm run dev -- --port ${TEST_PORT} --webpack`,
     url: `http://localhost:${TEST_PORT}`,
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
